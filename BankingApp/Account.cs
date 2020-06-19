6182d3b7473bfa9248ce14d6ff7a9f1f892f467f
max-student <max-student@maxtrain.com>
2020-06-19 13:54:50 -0400

    Made more changes to Banking Application.

--- BankingApp/Account.cs
+++ BankingApp/Account.cs
@@ -5,39 +5,84 @@ using System.Text;
 namespace BankingApp {
     class Account {
 
+        private bool IsAmountNegative(double Amount) {
+            if (Amount <= 0) {
+                Console.WriteLine("Amount must be positive.");
+                return true;
+            }
+            return false;
+        }
+
         // get is when someone is reading the data. get is left side.
         //set is when someone is making the data. set is right side. 
 
-        public int AccountNumber { get; set; }
-        public double Balance { get; set; } = 0;
-        
+        private static int NextAccountNumber = 1;
+        public int AccountNumber { get; private set; }
+        private double Balance { get; set; } = 0;
         public string Description { get; set; }
 
-        public void Deposit(double Amount) {
-            if (Amount <= 0) {
-                Console.WriteLine("Amount must be positive.");
-                Balance += Amount;
+        private static Account[] Accounts = new Account[5];
+        private static int NextIndex = 0;
+
+        public static void AddAccount(Account account) {
+            Accounts[NextIndex] = account;
+            NextIndex++;
+        }
+
+        public static void ListAccounts() {
+            for (var idx = 0; idx < NextIndex; idx++) {
+                var account = Accounts[idx];
+                Console.WriteLine($"Id:{account.AccountNumber}; Desc:{account.Description};            Bal:{account.Balance}");
             }
         }
-        public void Withdraw(double Amount) {
-            if (Amount < 0) {
-                Console.WriteLine("Amount must be positive.");
+
+        public bool Transfer(Account ToAccount, double Amount) {
+            var success = Withdraw(Amount);
+           if(!success) {
+                Console.WriteLine("Transfer failed - See log file");
+                return false;
+            }
+            success = ToAccount.Deposit(Amount);
+            if(!success) {
+                Console.WriteLine("Transfer failed -See log file");
+                Deposit(Amount);
+                return false;
+            }
+            return false;
+        }
+
+        public bool Deposit(double Amount) {
+            if(IsAmountNegative(Amount)) {
+                return false;
+            }
+
+            Balance += Amount;
+            return true;
+        }
+        public bool Withdraw(double Amount) {
+            if (IsAmountNegative(Amount)) {
+                return false;
             }
 
             if (Amount > Balance) {
                 Console.WriteLine("INSUFFICIENT FUNDS");
-                return;
+                return false;
             }
-
+            
             Balance -= Amount;
+            return true;
         }
+
+        public static string GetRoutingNumber() {
+            return "123 456 789";
+        }
+
         public double GetBalance() {
             return Balance;
         }
         public Account() {
-            this.AccountNumber = 0;
+            this.AccountNumber = NextAccountNumber++;
             this.Description = "New Account";
-            this.AccountNumber = 0;
         }
 
     }
--- BankingApp/Program.cs
+++ BankingApp/Program.cs
@@ -2,8 +2,10 @@
 
 namespace BankingApp {
     class Program {
+
         static void Main(string[] args) {
 
+            Console.WriteLine($"Routing number is {Account.GetRoutingNumber()}");
 
             var acct1 = new Account();
             acct1.Deposit(100000);
@@ -12,7 +14,14 @@ namespace BankingApp {
             acct1.Deposit(-20000);
             acct1.Withdraw(-10000);
 
+            var acct2 = new Account();
+            acct1.Transfer(acct2, 1000);
+            acct1.Transfer(acct2, 40000);
+            acct1.Transfer(acct2, 5000);
 
+            Account.AddAccount(acct1);
+            Account.AddAccount(acct2);
+            Account.ListAccounts();
         }
     }
 }
